f76356858a1c3ce25cd37672d4127a88c4f437ec
Ferdie De Oliveira <ferdie.deoliveira@siliconpractice.co.uk>
2020-02-18 12:58:23 +0000

    Working on ability to display events

---
+++ resources/js/components/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <events-list :events="events"></events-list>
+    <event-container :event="event"></event-container>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+  data: () => {
+    return {
+      events: [],
+      event: {}
+    };
+  }
+};
+</script>
